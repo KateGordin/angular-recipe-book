d5af1a0c07e9c8b3c7e5ee9222ae6433a80cd0a8
KateGordin <kate.gordin@gmail.com>
2022-07-22 16:07:47 +0200

    Refactor

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -6,8 +6,7 @@ import {Directive, HostBinding, ElementRef, HostListener} from "@angular/core";
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
   @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
-    console.log(event.target);
-    console.log(this.elRef.nativeElement, 'this.elRef.nativeElement');
+
     this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
 
