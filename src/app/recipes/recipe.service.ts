b70dbf0afe4bca44efe4c17fb8c8ceaaa1f6c6ce
KateGordin <kate.gordin@gmail.com>
2022-07-27 12:45:38 +0200

    Use a service for cross-component communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import {EventEmitter} from "@angular/core";
+
 import {Recipe} from "./recipe.model";
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://www.simplyrecipes.com/thmb/JWjdE8YwikAae0KZuyy6ZJW7Utw=/3000x2001/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg'),
     new Recipe('Another Test Recipe', 'This is simply a test', 'https://static.toiimg.com/thumb/53110049.cms?width=1200&height=900'),
